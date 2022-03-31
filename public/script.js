788e5b98031db93ca805b606850082c46a8a1481
Igor Bittencourt <igor.bitt.g@gmail.com>
2022-03-31 18:28:25 -0300

    Corrigidos bugs de instanciação e atualização de gráfio. Erros ocorriam porque o script tentava instanciar um objeto já criado, e não era atualizado pois não havia chamada para função de atualização do gráfico

--- public/script.js
+++ public/script.js
@@ -1,20 +1,13 @@
-const DEFAULT_URL = "localhost"
-
-function renderSensorData(id, data, unit){
-    document.getElementById(id).innerHTML = data + unit;
-}
-
-function renderChart(dataToChart){
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: ['18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00'],
         datasets: [{
             cubicInterpolationMode: 'monotone',
             tension: 5,
+            data: [],
             label: 'Temperatura',
-            data: dataToChart,
             backgroundColor: [
                 'rgba(48, 134, 219, 0.5)'],
             borderColor: [
@@ -30,15 +23,36 @@ function renderChart(dataToChart){
             }
         }
     });
+
+function renderStationData(id, data, unit){
+    document.getElementById(id).innerHTML = data + unit;
 }
 
-var socket = io(DEFAULT_URL);
+function renderChart(dataToChart){
+    myChart.data = {
+        labels: ['18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00'],
+        datasets: [{
+            cubicInterpolationMode: 'monotone',
+            tension: 5,
+            data: dataToChart,
+            label: 'Temperatura',
+            backgroundColor: [
+                'rgba(48, 134, 219, 0.5)'],
+            borderColor: [
+                'rgba(48, 134, 219, 1)',],
+            borderWidth: 1
+        }]
+    }
+    myChart.update();
+}
+
+var socket = io(window.location.hostname);
 
 socket.on('stationData', function(stationData){
-    renderSensorData("minTemperature", stationData.TempMin, "°C");
-    renderSensorData("currentTemperature", stationData.TempCurrent, "°C");
-    renderSensorData("maxTemperature", stationData.TempMax, "°C");
-    renderSensorData("pressure", stationData.pressure, " hPa");
-    renderSensorData("city", stationData.location, "");
-    renderChart(SensorData.chartData);
+    renderStationData("minTemperature", stationData.TempMin, "°C");
+    renderStationData("currentTemperature", stationData.TempCurrent, "°C");
+    renderStationData("maxTemperature", stationData.TempMax, "°C");
+    renderStationData("pressure", stationData.pressure, " hPa");
+    renderStationData("localization", stationData.location, "");
+    renderChart(stationData.chartData);
 }); 
